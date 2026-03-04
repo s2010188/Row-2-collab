dd1ec4c1e803c6d7cb25dc52c7ab65e751a66447
2010083 <s2010083@usls.edu.ph>
2026-03-04 15:53:47 +0800

    First Commit

---
+++ src/ReignClass.java
@@ -0,0 +1,9 @@
+public class ReignClass {
+    public static void main (String[] args ){
+        int el = 0;
+        int L = 0;
+        String firstName, lastName;
+
+        System.out.println("Hi Reign");
+    }
+}
